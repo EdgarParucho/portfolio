e676bdd0f7bfc4b8845aba7767e0724e4a004e9f
EdgarParucho <parucho.edgar@outlook.es>
2022-03-02 18:02:20 -0400

    projects interface created. - View structure, images, text, and route created. - Avatar modified.

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
-
+import Projects from '../views/Projects.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -15,6 +15,11 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projects
   }
 ]
 
